f1fb91b704f5bf2aef245f499ed3ef2e233e23a4
Dan <futurenoird@gmail.com>
2019-02-27 06:08:22 +0100

    sql actualizado

--- tienda_online.sql
+++ tienda_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2019 a las 06:48:26
+-- Tiempo de generación: 27-02-2019 a las 06:06:00
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -46,10 +46,10 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id_producto`, `nombre`, `anno`, `genero`, `formato`, `precio`, `unidades`, `imagen`, `descripcion`, `muestra`) VALUES
-(1, 'Jinjer - Micro', 2019, 'Metal', 'CD', 8.99, 39, 'web/resources/img_productos/jinjer_micro.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/QfhLma7gtPg\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
-(3, 'Nemix - The Planet', 2015, 'DarkSynth', 'Digital', 5, 22, 'web/resources/img_productos/nemix.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/xagptShRJlI\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
+(1, 'Jinjer - Micro', 2019, 'Metal', 'CD', 8.99, 40, 'web/resources/img_productos/jinjer_micro.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/QfhLma7gtPg\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
+(3, 'Nemix - The Planet', 2015, 'DarkSynth', 'Digital', 5, 0, 'web/resources/img_productos/nemix.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/xagptShRJlI\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
 (4, 'Skunk D.F. - Esencia', 2007, 'Rock', 'CD', 7.9, 24, 'web/resources/img_productos/uskunk5b35d.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/q_4v_9ERjeY\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
-(6, 'Cult of Luna & Julie Christmas - Cygnus (Perturbator remix)', 2018, 'Synth', 'Digital', 0.5, -10, 'web/resources/img_productos/perturbator-remix.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/gv7hFegk14o\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
+(6, 'Cult of Luna & Julie Christmas - Cygnus (Perturbator remix)', 2018, 'Synth', 'Digital', 0.5, 0, 'web/resources/img_productos/perturbator-remix.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/gv7hFegk14o\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
 (7, 'Carpenter Brut - EP II', 2013, 'Synthwave', 'Digital', 9.99, 0, 'web/resources/img_productos/CBEPII.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/qFfybn_W8Ak\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
 (8, 'Dynatron - Aeternus', 2015, 'Synthwave', 'Digital', 12, 0, 'web/resources/img_productos/a1829196462_16.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/F1rbVi6cYyA\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>'),
 (9, 'Rise Of The Northstar - Welcame', 2015, 'Metal', 'CD', 14.99, 28, 'web/resources/img_productos/ryseofthenorthstart.jpg', '', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/IW_y31bRW3M\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>');
