0d8d3bab017ea7c53cd08a911f113d897b3f4765
Dan <futurenoird@gmail.com>
2019-02-27 06:50:08 +0100

    Mejoras visuales y arreglado un bug en el formato de producto al editar

--- web/scripts/js/formato_producto.js
+++ web/scripts/js/formato_producto.js
@@ -2,18 +2,26 @@ window.onload = function () {
     let inputFormato = document.getElementById("formato")
     let inputCantidad = document.getElementById("unidades")
 
+    //Si el input ya existe en al edicion del producto..
+    if (inputFormato.value == "Digital") {
+        inputCantidad.min = 0;
+        inputCantidad.max = 0
+        inputCantidad.value = 0
+    }
+
+    //Si el input es pulsado en el registro de producto o en la edicion de este...
     inputFormato.onclick = function () {
         let formato = this.value;
-        
-        if (formato == "Digital") {
-           inputCantidad.min = 0;
-           inputCantidad.max = 0
-           inputCantidad.value = 0
 
-        }else if (formato == "CD"){
-           inputCantidad.min = 0;
-           inputCantidad.max = 100;
-           //inputCantidad.value = ""
+        if (formato == "Digital") {
+            inputCantidad.min = 0;
+            inputCantidad.max = 0
+            inputCantidad.value = 0
+
+        } else if (formato == "CD") {
+            inputCantidad.min = 0;
+            inputCantidad.max = 100;
+            //inputCantidad.value = ""
         }
     }
 }
